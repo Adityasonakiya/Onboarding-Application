d5ed1dfd8129bbf29b3c8cc52ba5978368c926ac
Aaditya <aaditya.v@somaiya.edu>
2025-02-19 21:45:52 +0530

    landing page

--- backend/src/main/java/com/example/onboarding/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/onboarding/repository/EmployeeRepository.java
@@ -16,11 +16,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query(value = "SELECT emp.psid as id, emp.first_name, emp.last_name, lob.lob_name, selection.hsbchiring_manager, obs.onboarding_status, bgvs.bgv_status " +
                    "FROM employee emp, lob lob, selection_details selection, onboarding_status obs, BGVStatus bgvs, tagging_details td " +
                    "WHERE selection.created_by = 10713037 AND selection.ps_id = emp.psid AND selection.lob_id = lob.lob_id " +
-                   "AND emp.psid = td.ps_id AND td.onboarding_status_id = obs.status_id AND td.bgvstatus_id = bgvs.bgv_status_id " +
+                   "AND td.onboarding_status_id = obs.status_id AND td.bgvstatus_id = bgvs.bgv_status_id " +
                    "UNION " +
                    "SELECT cnd.candidate_id as id, cnd.first_name, cnd.last_name, lob.lob_name, selection.hsbchiring_manager, obs.onboarding_status, bgvs.bgv_status " +
                    "FROM candidate cnd, lob lob, selection_details selection, onboarding_status obs, BGVStatus bgvs, tagging_details td " +
                    "WHERE selection.created_by = 10713037 AND selection.candidate_id = cnd.candidate_id AND selection.lob_id = lob.lob_id " +
-                   "AND cnd.candidate_id = td.candidate_id AND td.onboarding_status_id = obs.status_id AND td.bgvstatus_id = bgvs.bgv_status_id", nativeQuery = true)
+                   "AND td.onboarding_status_id = obs.status_id AND td.bgvstatus_id = bgvs.bgv_status_id", nativeQuery = true)
     List<EmployeeCandidateDTO> findEmployeeCandidates();
 }
