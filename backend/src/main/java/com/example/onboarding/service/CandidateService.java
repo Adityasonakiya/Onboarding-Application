979014d3df48ba11421758f62882aa2f9b778dcd
Harshita-189 <t.harshita1892@gmail.com>
2025-08-22 14:31:15 +0530

    bug fixes

--- backend/src/main/java/com/example/onboarding/service/CandidateService.java
+++ backend/src/main/java/com/example/onboarding/service/CandidateService.java
@@ -48,7 +48,7 @@ public class CandidateService {
 
     public Candidate createCandidate(Candidate candidate) {
         System.out.println("ServicePoint " + candidate);
-        candidate.setCandidateId(1);
+        candidate.setVendorId(1);
         candidate.setCreateDate(new Date());
         candidate.setUpdateDate(new Date());
         //candidate.setCreatedBy(employeeRepository.findById(userService.loggedUser().getPsid()).get());
